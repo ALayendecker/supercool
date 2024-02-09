b26b0c3760cdaea7fee6f90953dfb366db7a1bb0
ALayendecker <andrewlayendecker@gmail.com>
2024-02-09 11:44:44 -0600

    removed server

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true,
-    proxy: {
-      '/graphql': {
-        target: 'https://testing-gql-vite.onrender.com',
-        secure: false,
-        changeOrigin: true,
-      }
-    }
-  }
 })
