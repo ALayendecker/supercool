2aac8cea47280033e5e487ad056c49a4fa5ac7cf
ALayendecker <andrewlayendecker@gmail.com>
2024-02-09 10:52:12 -0600

    added render to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import Navbar from './components/Navbar';
 
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: 'https://testing-gql-vite.onrender.com/graphql',
 });
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
